744d12d58aab4bf7f7c8f88e717e55562f0f9e97
YYW <2955424225@qq.com>
2026-05-06 18:10:33 +0800

    测试提交

--- sky-common/src/main/java/com/sky/json/JacksonObjectMapper.java
+++ sky-common/src/main/java/com/sky/json/JacksonObjectMapper.java
@@ -31,6 +31,7 @@ public class JacksonObjectMapper extends ObjectMapper {
 
     public JacksonObjectMapper() {
         super();
+
         //收到未知属性时不报异常
         this.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
 
