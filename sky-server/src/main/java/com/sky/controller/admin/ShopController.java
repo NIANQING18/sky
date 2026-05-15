327ac8e79d32df9c1af12f8d93dec6c6ca912e9a
YYW <2955424225@qq.com>
2026-05-15 08:45:13 +0800

    配置Redis和添加店铺状态控制功能

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,44 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+@Slf4j
+@RequestMapping("/admin/shop")
+@Api("店铺状态接口")
+public class ShopController {
+
+    private static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    /**
+     * 设置店铺状态
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺状态")
+    public Result<Void> setStatus(@PathVariable(value = "status") Integer status){
+        redisTemplate.opsForValue().set(KEY,status);
+        return Result.success();
+    }
+
+    /**
+     * 获取店铺状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺状态")
+    public Result<Integer> getStatus(){
+        Integer value = (Integer) redisTemplate.opsForValue().get(KEY);
+        return Result.success(value);
+    }
+}
---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,31 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userShopController")
+@Slf4j
+@RequestMapping("/user/shop")
+@Api("店铺状态接口")
+public class ShopController {
+    private static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    /**
+     * 获取店铺状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺状态")
+    public Result<Integer> getStatus(){
+        Integer value = (Integer) redisTemplate.opsForValue().get(KEY);
+        return Result.success(value);
+    }
+}
